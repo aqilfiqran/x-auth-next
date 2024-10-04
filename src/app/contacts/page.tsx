8cf528e83b74736552380e1153fac9c3987fad26
aqilfiqran <aqilfiqran19@gmail.com>
2024-10-04 15:53:59 +0700

    feat: sitemap generate

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
 import { Section } from './_components/Section';
+import { Loader } from 'lucide-react';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-info-semantic p-4">
-      <Section />
+      <Suspense fallback={<Loader className="animate-spin" />}>
+        <Section />
+      </Suspense>
     </main>
   );
 }
